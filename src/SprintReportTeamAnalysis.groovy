2ba2c08f5df3fc78f2918179682db63e3bb034e3
Uman, Howard <human@cvent.com>
2023-04-11 16:36:09 -0400

    Sprint Report now pulls comment data

--- src/SprintReportTeamAnalysis.groovy
+++ src/SprintReportTeamAnalysis.groovy
@@ -1,6 +1,9 @@
 import groovy.cli.commons.OptionAccessor
+import org.apache.commons.codec.binary.StringUtils
 
 class SprintReportTeamAnalysis extends AbstractSprintReport {
+    final List<String> IGNORE_USERS = [ "codeowners" ]
+    final List<String> IGNORE_COMMENTS = [ "Tasks to Complete Before Merging Pull Request" ]
     @Override
     def process(OptionAccessor commandLineOptions, String boardId, List<String> sprintIds) {
         sprintIds.each(sprintId -> {
@@ -20,7 +23,7 @@ class SprintReportTeamAnalysis extends AbstractSprintReport {
 
             def prInfo = jiraREST.getTicketPullRequestInfo(issueId.toString())
             prInfo.detail.each(prDetail -> {
-                System.out.println("${ticket} / ${issueId}  has ${prDetail.pullRequests.size()} PRs")
+                System.out.println("${ticket} / Issue ${issueId} has ${prDetail.pullRequests.size()} PRs")
                 prDetail.pullRequests.each(pullRequest -> {
                     // can get approvers out of ^^^
                     // check comment count before polling for comments
@@ -29,9 +32,60 @@ class SprintReportTeamAnalysis extends AbstractSprintReport {
                     def prUrl = pullRequest.url
 
                     def prActivities = bitbucketREST.getActivities(prUrl)
-                    System.out.println("   PR ${prId} has ${prActivities.values.size()} activities")
+                    System.out.println("   PR ${ticket} / ${prId} has ${prActivities.values.size()} activities")
+                    for (Object prActivity: prActivities.values) {
+                        String userName = prActivity.user.name
+                        if (IGNORE_USERS.contains(userName)) {
+                            continue
+                        }
+                        String prActivityAction = prActivity.action
+                        switch (prActivityAction) {
+                            case "APPROVED":
+                                System.out.println("   ${userName} ${prActivityAction}")
+                                continue
+                            case "COMMENTED":
+                                processComment(userName, prActivity)
+                                continue
+                            case "DECLINED":
+                                // TODO Track this
+                                continue
+                            case "OPENED":
+                                System.out.println("   ${userName} ${prActivityAction}")
+                                continue
+                            case "MERGED":
+                                System.out.println("   ${userName} ${prActivityAction}")
+                                continue
+                            case "RESCOPED":
+                                // do nothing
+                                continue
+                            case "UNAPPROVED":
+                                // TODO Revisit - do nothing?
+                                continue
+                            case "UPDATED":
+                                // do nothing
+                                continue
+                            default:
+                                System.out.println("   ${userName} Unhandled action: ${prActivityAction}")
+                        }
+                    }
                 })
             })
+        })
+    }
+
+    def processComment(String userName, Object prActivity) {
+        processComment(userName, prActivity.action, prActivity.commentAction, prActivity.comment, 3)
+    }
+
+    def processComment(String userName, String action, String commentAction, Object comment, int indentation) {
+        String commentText = comment.text
+        if (IGNORE_COMMENTS.contains(commentText)) {
+            return
+        }
+        commentText = commentText.replaceAll("(\\r|\\n)?\\n", "  ").trim()
+        System.out.println("${org.apache.ivy.util.StringUtils.repeat(' ', indentation)}${userName} ${action} ${commentAction}: ${commentText}")
+        comment.comments.forEach(replyComment -> {
+            processComment(replyComment.author.name, "COMMENTED", "REPLY", replyComment, indentation + 3)
         })
     }
 }
